24140be0e5aab56c09c0bd5b8bf1f208a6882e07
Jordan Hamel <jordan@nearbuysystems.com>
2013-06-28 14:20:07 -0700

    Can edit photo tags

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,16 +7,13 @@ class PhotosController < ApplicationController
 
   def create
     file = params[:photo_file].tempfile.read
-    puts file.inspect
 
 		photo = Photo.new(params[:photo])
 		photo.file = file
 
     if photo.save
-      count = Tag.all.count
-      (1..count).each do |x|
-        if params["tag#{x}"]
-          tag = Tag.find(x)
+      Tag.all.each do |tag|
+        if params["tag#{tag.id}"]
           if tag
             PhotoTag.create(
               :photo_id => photo.id,
@@ -33,12 +30,47 @@ class PhotosController < ApplicationController
     redirect_to root_path
   end
 
+  def update_tags
+    photo = Photo.find(params[:id])
+    if photo
+
+      PhotoTag.all.each do |pt|
+        if pt.photo_id == photo.id
+          pt.delete
+        end
+      end
+
+      Tag.all.each do |tag|
+        if params["tag#{tag.id}"]
+          if tag
+            PhotoTag.create(
+              :photo_id => photo.id,
+              :tag_id => tag.id,
+            )
+          end
+        end
+      end
+    end
+
+    redirect_to photos_path
+  end
+
   def index
     @photos = Photo.all
   end
 
   def show
     @photo = Photo.find(params[:id])
+    @tags = Tag.all
+
+    @checked = {}
+    @tags.each do |tag|
+      if @photo.tags.include?(tag)
+        @checked[tag.id] = true
+      else
+        @checked[tag.id] = nil
+      end
+    end
   end
 
   def show_file
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Clothing::Application.routes.draw do
   resources :photos, :only => [:new, :create, :index, :show]
 
   get '/photos/:id/show_file', to: 'photos#show_file'
+  post '/photos/:id/update_tags', to: 'photos#update_tags'
 end
