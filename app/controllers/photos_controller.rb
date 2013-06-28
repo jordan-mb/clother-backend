97fab6a72bbd635cc68cbda896fe306d941e1940
Jordan Hamel <jordan@nearbuysystems.com>
2013-06-27 21:53:12 -0700

    Photo upload form with tags

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,22 +1,48 @@
 class PhotosController < ApplicationController
-  before_filter :authenticate_user!
+  #before_filter :authenticate_user!
 
   def new
-    @photo = Photo.new
+    @tags = Tag.all
   end
 
   def create
-    file = params[:photo].delete(:file).read
+    file = params[:photo_file].tempfile.read
+    puts file.inspect
 
-		@photo = Photo.new(params[:photo])
-		@photo.file = file
+		photo = Photo.new(params[:photo])
+		photo.file = file
 
-		if @photo.save
-			flash[:success] = "Your photo has been successfully uploaded!"
-			redirect_to root_path
-		else
-			flash[:error] = "Well, that didn't work! Care to try again? (Your file might be too big)"
-			render 'new'
-		end
+    if photo.save
+      count = Tag.all.count
+      (1..count).each do |x|
+        if params["tag#{x}"]
+          tag = Tag.find(x)
+          if tag
+            PhotoTag.create(
+              :photo_id => photo.id,
+              :tag_id => tag.id,
+            )
+          end
+        end
+      end
+
+    else
+      flash[:error] = "Photo did not save"
+    end
+
+    redirect_to root_path
+  end
+
+  def index
+    @tagged = []
+    @untagged = []
+
+    Photo.all.each do |p|
+      if p.tags.length > 0
+        @tagged << p
+      else
+        @untagged << p
+      end
+    end
   end
 end
