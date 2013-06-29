624842f2d224089b70af235e5e75dc447ec614a8
Jordan Hamel <jordan@nearbuysystems.com>
2013-06-28 23:05:00 -0700

    User index & user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    preferences = {} #tag => value
+    UserTag.where(:user_id => params[:id]).each do |ut|
+      tag = Tag.find(ut.tag_id)
+      tag = tag.name if tag
+      preferences[tag] = ut.value
+    end
+
+    @preferences = preferences.sort_by {|k, v| v}.reverse
+  end
 end
