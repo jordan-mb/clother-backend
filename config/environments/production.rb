5bfd6beaf4e05c2cf31389007abbc4a675fc7c3f
Jordan Hamel <jordan@nearbuysystems.com>
2013-07-01 15:58:51 -0700

    Gem and environment for assest sync

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,7 @@ Clothing::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  config.assets.enabled = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
@@ -64,4 +65,6 @@ Clothing::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
 end
