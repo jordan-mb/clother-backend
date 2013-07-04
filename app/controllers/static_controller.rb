260ce61f652a214d11dd6aa50d86acb2d8af4730
Jordan Hamel <jordan@nearbuysystems.com>
2013-07-03 18:13:17 -0700

    Reverting "temp admin button for jo"
    This reverts commit 7cd81efd0779c36b78d67406eb3d0e881367acaf.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,10 +1,2 @@
 class StaticController < ApplicationController
-  def make_jo_admin
-    jos = User.where(:email => "jo.mravca@gmail.com")
-    jo = jos[0]
-    jo.admin = true
-    jo.save!
-
-    redirect_to root_path
-  end
 end
