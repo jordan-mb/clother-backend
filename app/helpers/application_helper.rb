fcdff20aa142729b908d8704c696a3d44d43e644
Jordan Hamel <jordan@nearbuysystems.com>
2013-07-05 16:28:19 -0700

    Modify s3_path helper, and use for photos stylesheet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def s3_path(img)
-    "http://s3-us-west-2.amazonaws.com/clotherphotos/assets/#{img}"
+  def s3_path(file)
+    "https://s3-us-west-2.amazonaws.com/clotherphotos/assets/#{file}"
   end
 end
